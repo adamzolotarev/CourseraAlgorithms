266629445a37c4792021dd7b0110bd30258426df
eugenezhub <eugenezolo@gmail.com>
2015-06-27 22:01:29 -0400

    two dim array

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,9 +1,9 @@
 public class Percolation {
-    private boolean[] sites;
     private int gridLength;
     private QuickUnionUF sortingAlgorithm;
     private QuickUnionUF sortingAlgorithmForFull;
     private int lastSiteIndex;
+    private boolean[][] openSites;
 
     public Percolation(int gridLength) {
         if (gridLength <= 0) throw new java.lang.IllegalArgumentException();
@@ -21,70 +21,72 @@ public class Percolation {
     private void initialize() {
         int numberOfVirtualSites = 2;
         int numberOfSites = gridLength * gridLength + numberOfVirtualSites;
-        sites = new boolean[numberOfSites];
-        lastSiteIndex = sites.length -1;
-        sites[0] = true;
-        sites[lastSiteIndex] = true;
+        lastSiteIndex = numberOfSites - 1;
         sortingAlgorithm = new QuickUnionUF(numberOfSites);
         sortingAlgorithmForFull = new QuickUnionUF((numberOfSites - 1));
+        openSites = new boolean[gridLength][gridLength];
+    }
+
+    private int GetIndex(int row, int column, int retrievedIndex) {
+        if (retrievedIndex > 0) {
+            return retrievedIndex;
+        }
+        return getSiteIndexFromCoordinates(row, column);
     }
 
     public void open(int row, int column) {
         validateRowColumn(row, column);
-        int correspondingSiteIndex = getSiteIndexFromCoordinates(row, column);
-        boolean correspondingSite = sites[correspondingSiteIndex];
-        if (correspondingSite) return;
+        int adjustedRow = row - 1;
+        int adjustedColumn = column - 1;
+        if (openSites[adjustedRow][adjustedColumn]) return;
+        int correspondingSiteIndex = 0;
 
         if (isFirstRow(row)) {
+            correspondingSiteIndex = GetIndex(row, column, correspondingSiteIndex);
             connectSites(0, correspondingSiteIndex);
-            int bottom = getSiteIndexFromCoordinates(row + 1, column);
-            if(sites[bottom]) {
-                connectSites(bottom, correspondingSiteIndex);
+            if (gridLength > 1 && openSites[adjustedRow + 1][adjustedColumn] || gridLength == 1) {
+                connectSites(getSiteIndexFromCoordinates(row + 1, column), correspondingSiteIndex);
             }
-        }
-        else if(isLastRow(row)){
+        } else if (isLastRow(row)) {
+            correspondingSiteIndex = GetIndex(row, column, correspondingSiteIndex);
             connectSites(lastSiteIndex, correspondingSiteIndex);
-            connectSites(0, correspondingSiteIndex);
-            int top = getSiteIndexFromCoordinates(row - 1, column);
-            if(sites[top]) {
-                connectSites(top, correspondingSiteIndex);
+            if (gridLength > 1 && openSites[adjustedRow - 1][adjustedColumn]) {
+                connectSites(getSiteIndexFromCoordinates(row - 1, column), correspondingSiteIndex);
             }
-        }
-        else{
-            int bottom = getSiteIndexFromCoordinates(row + 1, column);
-            if(sites[bottom]) {
-                connectSites(bottom, correspondingSiteIndex);
+        } else {
+            if (openSites[adjustedRow + 1][adjustedColumn]) {
+                correspondingSiteIndex = GetIndex(row, column, correspondingSiteIndex);
+                connectSites(correspondingSiteIndex + gridLength, correspondingSiteIndex);
             }
-            int top = bottom -2*gridLength;
-            if(sites[top]) {
-                connectSites(top, correspondingSiteIndex);
+            if (openSites[adjustedRow - 1][adjustedColumn]) {
+                correspondingSiteIndex = GetIndex(row, column, correspondingSiteIndex);
+                connectSites(correspondingSiteIndex - gridLength, correspondingSiteIndex);
             }
         }
 
-        if (column == gridLength) {
-            int leftSide = getSiteIndexFromCoordinates(row, column - 1);
-            if (sites[leftSide]) {
-                connectSites(leftSide, correspondingSiteIndex);
+        if (gridLength > 1) {
+            if (column == gridLength) {
+                if (openSites[adjustedRow][adjustedColumn - 1]) {
+                    correspondingSiteIndex = GetIndex(row, column, correspondingSiteIndex);
+                    connectSites(getSiteIndexFromCoordinates(row, column - 1), correspondingSiteIndex);
+                }
+            } else if (column == 1) {
+                if (openSites[adjustedRow][adjustedColumn + 1]) {
+                    correspondingSiteIndex = GetIndex(row, column, correspondingSiteIndex);
+                    connectSites(getSiteIndexFromCoordinates(row, column + 1), correspondingSiteIndex);
+                }
+            } else {
+                if (openSites[adjustedRow][adjustedColumn - 1]) {
+                    correspondingSiteIndex = GetIndex(row, column, correspondingSiteIndex);
+                    connectSites(correspondingSiteIndex - 1, correspondingSiteIndex);
+                }
+                if (openSites[adjustedRow][adjustedColumn + 1]) {
+                    correspondingSiteIndex = GetIndex(row, column, correspondingSiteIndex);
+                    connectSites(correspondingSiteIndex + 1, correspondingSiteIndex);
+                }
             }
         }
-        else if(column ==1) {
-            int rightSide = getSiteIndexFromCoordinates(row, column + 1);
-            if (sites[rightSide]) {
-                connectSites(rightSide, correspondingSiteIndex);
-            }
-        }
-        else {
-            int leftSide = getSiteIndexFromCoordinates(row, column - 1);
-            if (sites[leftSide]) {
-                connectSites(leftSide, correspondingSiteIndex);
-            }
-            int rightSide = leftSide + 2;
-            if (sites[rightSide]) {
-                connectSites(rightSide, correspondingSiteIndex);
-            }
-        }
-
-        sites[correspondingSiteIndex] = true;
+        openSites[adjustedRow][adjustedColumn] = true;
     }
 
     private void connectSites(int neighborSiteIndex, int correspondingSiteIndex) {
@@ -96,7 +98,7 @@ public class Percolation {
 
     public boolean isOpen(int row, int column) {
         validateRowColumn(row, column);
-        return sites[getSiteIndexFromCoordinates(row, column)];
+        return openSites[row - 1][column - 1];
     }
 
     public boolean isFull(int row, int column) {
@@ -105,9 +107,8 @@ public class Percolation {
     }
 
     private boolean isFullWithoutValidation(int row, int column) {
+        if (!openSites[row - 1][column - 1]) return false;
         int correspondingSiteIndex = getSiteIndexFromCoordinates(row, column);
-        if (!sites[correspondingSiteIndex]) return false;
-
         return sortingAlgorithmForFull.connected(0, correspondingSiteIndex);
     }
 
